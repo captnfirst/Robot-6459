394681d4847b33d9c40a0674c829894555b5e543
Bahadır <bahadirtilki@outlook.com>
2023-06-09 20:29:24 +0300

    main

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,461 @@
+// Samet <3 Gelecekteki yengemiz...
+// İbrahim <3 Gelecekteki yengemiz...
+// Akif <3 Bitlis
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.Turret;
+
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+  private RobotContainer m_robotContainer;
+
+  AddressableLED led = new AddressableLED(0);
+  AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(78);
+  int id;
+  int hue;
+  Timer ledBlinkTimer = new Timer();
+  double blinkTime = 0.1;
+
+  Joystick driverJoystick = new Joystick(0);
+  Joystick yardimciJoystick = new Joystick(1);
+
+  Timer backlashTimer = new Timer();
+  Timer teleoptime = new Timer();
+  double speedArmPosThreshold = 10000;
+
+  double kDriveSpeed = 0.87;
+  double kRotationSpeed = 0.56;
+
+  int desiredPos = 0;
+  int gamePiece = 0; // -1 = cube / 0 = Rainbow / 1 = cone // 2 = ReadyToShoot // 3 = last 20 sec
+  boolean autoResetStatus = false;
+
+  double armAngleOffset_humanPlayer = 0;
+
+  @Override
+  public void robotInit() {
+    m_robotContainer = new RobotContainer();
+    m_robotContainer.m_drive.gyro.reset();
+    CameraServer.startAutomaticCapture();
+  }
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+    setLeds(gamePiece);
+  }
+
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+    gamePiece = 0;
+    m_robotContainer.m_intake.setIntakeSolenoid(Value.kOff);
+  }
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    autoResetStatus = true;
+  }
+
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    teleoptime.start();
+    if(teleoptime.get()>115){
+      gamePiece = 3; // balance red
+    }
+    
+    backlashTimer.start();
+    m_robotContainer.m_drive.drive.arcadeDrive(driverJoystick.getRawAxis(1) * kDriveSpeed,
+        driverJoystick.getRawAxis(2) * kRotationSpeed);
+    if (desiredPos == -1) { // human player
+      kDriveSpeed = 0.5;
+      kRotationSpeed = 0.5;
+    } else if (desiredPos == 0) { // transport
+      kDriveSpeed = 0.87;
+      kRotationSpeed = 0.56;
+    } else if (desiredPos == 1) { // 1lvl
+      kDriveSpeed = 0.78;
+      kRotationSpeed = 0.54;
+    } else if (desiredPos == 2 || desiredPos == 3) { // 2&3 lvl
+      kDriveSpeed = 0.5;
+      kRotationSpeed = 0.52;
+    } else if (desiredPos == 4) { // ground
+      kDriveSpeed = 0.48;
+      kRotationSpeed = 0.48;
+    }
+
+    if (yardimciJoystick.getRawButton(2)) {
+      desiredPos = 0; // transport
+    } else if (yardimciJoystick.getRawButton(1)) {
+      desiredPos = -1; // human
+    } else if (yardimciJoystick.getRawButton(4)) {
+      desiredPos = 1; // 1 lvl
+    } else if (yardimciJoystick.getRawButton(3)) {
+      desiredPos = 2; // 2 lvl
+    } else if (yardimciJoystick.getRawButton(12)) {
+      desiredPos = 3; // 3 lvl
+    } else if (yardimciJoystick.getRawButton(11)) {
+      desiredPos = 4; // ground
+    }
+
+    if (yardimciJoystick.getPOV() == 0) {
+      if (desiredPos == -1) {
+        armAngleOffset_humanPlayer += 0.1;
+      }
+    } else if (yardimciJoystick.getPOV() == 180) {
+      if (desiredPos == -1) {
+        armAngleOffset_humanPlayer -= 0.1;
+      }
+    }
+
+    if (yardimciJoystick.getRawButton(5)) {
+      m_robotContainer.m_intake.setIntakeSolenoid(Value.kReverse); // on
+    } else if (yardimciJoystick.getRawButton(6)) {
+      m_robotContainer.m_intake.setIntakeSolenoid(Value.kForward); // off
+    }
+
+    if (yardimciJoystick.getRawButton(7)) {
+      m_robotContainer.m_intake.intakeMotor.set(0.3);
+    } else if (yardimciJoystick.getRawButton(8)) {
+      if (desiredPos == 3 && gamePiece == -1) { // 3lvl cube
+        m_robotContainer.m_intake.setIntake(-0.72);
+      } else if (desiredPos == 3 && gamePiece == 1) { // 3lvl cone
+        m_robotContainer.m_intake.setIntake(-1);
+      } else if (desiredPos == 2 && gamePiece == -1) { // 2lvl cube
+        m_robotContainer.m_intake.setIntake(-0.72);
+      } else if (desiredPos == 2 && gamePiece == 1) { // 2lvl cone
+        m_robotContainer.m_intake.setIntake(-1);
+      } else if (desiredPos == 1 && gamePiece == -1) { // 1lvl cube
+        if (m_robotContainer.m_turret.getTurretPos() > -40 && m_robotContainer.m_turret.getTurretPos() < 40) {
+          m_robotContainer.m_intake.intakeMotor.stopMotor();
+        } else {
+          m_robotContainer.m_intake.intakeMotor.set(-1);
+        }
+      } else if (desiredPos == 1 && gamePiece == 1) { // 1lvl cone
+        if (m_robotContainer.m_turret.getTurretPos() > -40 && m_robotContainer.m_turret.getTurretPos() < 40) {
+          m_robotContainer.m_intake.intakeMotor.stopMotor();
+        } else {
+          m_robotContainer.m_intake.intakeMotor.set(-0.5);
+        }
+      } else {
+        m_robotContainer.m_intake.setIntake(0.1);
+      }
+    }
+
+    if (yardimciJoystick.getRawButton(10)) {
+      m_robotContainer.m_drive.gyro.reset();
+    }
+
+    if (yardimciJoystick.getPOV() == 90 && (desiredPos == 2 || desiredPos == 3)) {
+      m_robotContainer.m_turret.visionLockingStatus = true;
+      if (Turret.targetStatus == 1 && Turret.x > -1 && Turret.x < 1) {
+        gamePiece = 2; // ready
+      } else {
+        gamePiece = 1; // cone
+      }
+    } else {
+      m_robotContainer.m_turret.visionLockingStatus = false;
+      m_robotContainer.m_turret.visionIncrement = 0;
+      if (gamePiece == 2) { // ready
+        gamePiece = 1; // cone
+      }
+    }
+
+    if (driverJoystick.getRawButton(5)) {
+      gamePiece = 1; // cone
+    } else if (driverJoystick.getRawButton(6)) {
+      gamePiece = -1; // cube
+    }
+
+    if (backlashTimer.get() > 2) {
+      if (desiredPos == -1) {
+        m_robotContainer.m_elevator.setElevator(true, -0.5);
+        if (m_robotContainer.m_elevator.getElevatorPos() > -5) {
+          m_robotContainer.m_arm.setArm(true, 101.5 + armAngleOffset_humanPlayer); // 85 - 100
+          if (m_robotContainer.m_arm.getArmPos() > 30) {
+            m_robotContainer.m_head.setHead(true, -6000);
+          }
+        }
+      } else if (desiredPos == 0) { // Transport
+        if (m_robotContainer.m_arm.getArmPos() > 40) {
+          m_robotContainer.m_arm.setArm(true, 22.5);
+          m_robotContainer.m_head.setHead(true, 8500);
+          if (m_robotContainer.m_arm.getArmPos() < 25) {
+            m_robotContainer.m_turret.setTurret(true, 180);
+            if (m_robotContainer.m_turret.getTurretPos() > 170 && m_robotContainer.m_turret.getTurretPos() < 190) {
+              if (m_robotContainer.m_arm.getArmPos() > 20) {
+                m_robotContainer.m_elevator.setElevator(true, -32.5);
+              }
+            }
+          }
+        } else {
+          m_robotContainer.m_turret.setTurret(true, 180);
+          if (m_robotContainer.m_turret.getTurretPos() > 170 && m_robotContainer.m_turret.getTurretPos() < 190) {
+            m_robotContainer.m_arm.setArm(true, 22.5);
+            m_robotContainer.m_head.setHead(true, 8500);
+            if (m_robotContainer.m_arm.getArmPos() > 20) {
+              m_robotContainer.m_elevator.setElevator(true, -32.5);
+            }
+          }
+        }
+        m_robotContainer.m_turret.lockOnTarget(false, false);
+      } else if (desiredPos == 1) { // 1 lvl
+        m_robotContainer.m_elevator.setElevator(true, -0.5);
+        if (m_robotContainer.m_elevator.getElevatorPos() > -5) {
+          m_robotContainer.m_arm.setArm(true, -5);
+          m_robotContainer.m_turret.setTurret(true,
+              (m_robotContainer.m_turret.getTurretAbs() - m_robotContainer.m_turret.getTurningOffset()));
+          if (m_robotContainer.m_arm.getArmPos() < 20) {
+            m_robotContainer.m_head.setHead(true, 10500);
+          }
+        }
+        m_robotContainer.m_turret.lockOnTarget(false, false);
+      } else if (desiredPos == 2) { // 2lvl
+        m_robotContainer.m_elevator.setElevator(true, -0.5);
+        if (m_robotContainer.m_elevator.getElevatorPos() > -5) {
+          m_robotContainer.m_turret.lockOnTarget(true, m_robotContainer.m_turret.visionLockingStatus);
+          m_robotContainer.m_arm.setArm(true, 120);
+          if (m_robotContainer.m_arm.getArmPos() > 30) {
+            // m_robotContainer.m_head.setHead(true, -11500);
+            // m_robotContainer.m_head.setHead(true, -16300);
+            // m_robotContainer.m_head.setHead(true, -18000);
+            m_robotContainer.m_head.setHead(true, -16300);
+          }
+        }
+      } else if (desiredPos == 3) { // 3lvl
+        m_robotContainer.m_elevator.setElevator(true, -0.5);
+        if (m_robotContainer.m_elevator.getElevatorPos() > -5) {
+          m_robotContainer.m_turret.lockOnTarget(true, m_robotContainer.m_turret.visionLockingStatus);
+          m_robotContainer.m_arm.setArm(true, 120);
+          if (m_robotContainer.m_arm.getArmPos() > 30) {
+            m_robotContainer.m_head.setHead(true, -1000);
+          }
+        }
+      } else if (desiredPos == 4) { // ground
+        m_robotContainer.m_elevator.setElevator(true, -0.5);
+        if (m_robotContainer.m_elevator.getElevatorPos() > -5) {
+          m_robotContainer.m_arm.setArm(true, -5);
+          m_robotContainer.m_head.setHead(true, 12000);
+          if (m_robotContainer.m_arm.getArmPos() < 20) {
+            m_robotContainer.m_turret.setTurret(true, 0);
+          }
+        }
+        m_robotContainer.m_turret.lockOnTarget(false, false);
+      }
+
+    } else if (backlashTimer.get() < 1.95) {
+      m_robotContainer.m_head.headMotor.set(ControlMode.PercentOutput, 0.05);
+      m_robotContainer.m_arm.armMotor.set(ControlMode.PercentOutput, -0.05);
+      m_robotContainer.m_elevator.elevatorMotor.set(ControlMode.PercentOutput, 0.05);
+      m_robotContainer.m_head.headMotor.setSelectedSensorPosition(0);
+      m_robotContainer.m_arm.armMotor.setSelectedSensorPosition(0);
+      m_robotContainer.m_elevator.elevatorMotor.setSelectedSensorPosition(0);
+    } else {
+      m_robotContainer.m_head.headMotor.stopMotor();
+      m_robotContainer.m_arm.armMotor.stopMotor();
+      m_robotContainer.m_elevator.elevatorMotor.stopMotor();
+    }
+  }
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {
+
+  }
+
+  @Override
+  public void simulationInit() {
+  }
+
+  @Override
+  public void simulationPeriodic() {
+  }
+
+  void huePlus() {
+    hue++;
+    if (hue == 180) {
+      hue = 0;
+    } else {
+      ledBuffer.setHSV(id, hue, 255, 255);
+    }
+  }
+
+  void idPlus() {
+    id++;
+    if (id == 78) {
+      id = 0;
+    } else {
+      ledBuffer.setHSV(id, hue, 255, 255);
+    }
+  }
+
+  public void setLeds(int ledStatus) {
+    Alliance ally = DriverStation.getAlliance();
+    ledBlinkTimer.start();
+    if (ledStatus == 0) { // rainbow
+      idPlus();
+      huePlus();
+    } else if (ledStatus == -1) { // purple cube
+      if (ledBlinkTimer.get() < blinkTime) {
+        for (int i = 0; i < 78; i++) {
+          if (i > 42 && i < 79) {
+            ledBuffer.setRGB(i, 255, 0, 255);
+          } else if (i < 42 && ally.equals(Alliance.Red)) {
+            ledBuffer.setRGB(i, 255, 0, 0);
+          } else if (id < 42 && ally.equals(Alliance.Blue)) {
+            ledBuffer.setRGB(id, 0, 0, 255);
+          }
+        }
+      } else if (ledBlinkTimer.get() > blinkTime && ledBlinkTimer.get() < blinkTime * 2) {
+        for (int i = 0; i < 78; i++) {
+          ledBuffer.setRGB(i, 0, 0, 0);
+        }
+      } else {
+        ledBlinkTimer.reset();
+      }
+    } else if (ledStatus == 1) { // yellow cone
+      if (ledBlinkTimer.get() < blinkTime) {
+        for (int i = 0; i < 78; i++) {
+          if (i > 42 && i < 79) {
+            ledBuffer.setRGB(i, 255, 255, 0);
+          } else if (i < 42 && ally.equals(Alliance.Red)) {
+            ledBuffer.setRGB(i, 255, 0, 0);
+          } else if (id < 42 && ally.equals(Alliance.Blue)) {
+            ledBuffer.setRGB(id, 0, 0, 255);
+          }
+        }
+      } else if (ledBlinkTimer.get() > blinkTime && ledBlinkTimer.get() < blinkTime * 2) {
+        for (int i = 0; i < 78; i++) {
+          ledBuffer.setRGB(i, 0, 0, 0);
+        }
+      } else {
+        ledBlinkTimer.reset();
+      }
+    } else if (ledStatus == 2) { // green ready
+      if (ledBlinkTimer.get() < blinkTime) {
+        for (int i = 0; i < 78; i++) {
+          if (i > 42 && i < 79) {
+            ledBuffer.setRGB(i, 0, 255, 0);
+          } else if (i < 42 && ally.equals(Alliance.Red)) {
+            ledBuffer.setRGB(i, 255, 0, 0);
+          } else if (id < 42 && ally.equals(Alliance.Blue)) {
+            ledBuffer.setRGB(id, 0, 0, 255);
+          }
+        }
+      } else if (ledBlinkTimer.get() > blinkTime && ledBlinkTimer.get() < blinkTime * 2) {
+        for (int i = 0; i < 78; i++) {
+          ledBuffer.setRGB(i, 0, 0, 0);
+        }
+      } else {
+        ledBlinkTimer.reset();
+      }
+    } else if (ledStatus == 3) { // red balance
+      if (ledBlinkTimer.get() < blinkTime) {
+        for (int i = 0; i < 78; i++) {
+          if (i > 42 && i < 79) {
+            ledBuffer.setRGB(i, 255, 0, 0);
+          } else if (i < 42 && ally.equals(Alliance.Red)) {
+            ledBuffer.setRGB(i, 255, 0, 0);
+          } else if (id < 42 && ally.equals(Alliance.Blue)) {
+            ledBuffer.setRGB(id, 0, 0, 255);
+          }
+        }
+      } else if (ledBlinkTimer.get() > blinkTime && ledBlinkTimer.get() < blinkTime * 2) {
+        for (int i = 0; i < 78; i++) {
+          ledBuffer.setRGB(i, 0, 0, 0);
+        }
+      } else {
+        ledBlinkTimer.reset();
+      }
+    }
+  }
+
+  // boolean gyroOffsetLock = false;
+  // double gyroOffset = 0;
+  // double visionIncrement = 0;
+
+  // public void lockOnTarget(boolean enabled, boolean visionOn) {
+  // // System.out.println(getXVal());
+  // if (enabled) {
+  // if (gyroOffsetLock == false) {
+  // gyroOffset = -180;
+  // gyroOffsetLock = true;
+  // }
+  // if (visionOn && getArmPos() > 90) {
+  // visionIncrement += getXVal();
+  // setTurret(true, getProtectedTurretLockingAngle() - visionIncrement);
+  // } else {
+  // setTurret(true, getProtectedTurretLockingAngle());
+  // }
+  // } else {
+  // gyroOffsetLock = false;
+  // gyroOffset = 0;
+  // visionIncrement = 0;
+  // }
+  // }
+
+  // public double getTurretAbs() {
+  // double turningOffset = 180;
+  // return (gyroOffset - (-m_robotContainer.m_Superstructure.gyro.getAngle())) +
+  // turningOffset;
+  // }
+
+  // public double getProtectedTurretLockingAngle() {
+  // double valueToReturn;
+  // if (getTurretAbs() > 220) {
+  // gyroOffset -= 360;
+  // valueToReturn = getTurretAbs();
+  // } else if (getTurretAbs() < -220) {
+  // gyroOffset += 360;
+  // valueToReturn = getTurretAbs();
+  // } else {
+  // valueToReturn = getTurretAbs();
+  // }
+  // return valueToReturn;
+  // }
+
+  // public double getAngleVisionOffset() {
+  // double valueToReturn;
+  // valueToReturn = getTurretAbs() * 0.01;
+  // System.out.print(getTurretAbs());
+  // System.out.print(" ");
+  // System.out.println(valueToReturn);
+  // return valueToReturn;
+  // }
+
+}
